f122499e622c883741ecf49cd4e020f1ad5da27a
nightclassXCR <1246716434@qq.com>
2025-11-14 16:47:33 +0800

    FEAT:新增ToolCalling

--- src/main/java/com/dd/ai_medical_triage/config/AIConfig.java
+++ src/main/java/com/dd/ai_medical_triage/config/AIConfig.java
@@ -1,16 +1,31 @@
 package com.dd.ai_medical_triage.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static com.dd.ai_medical_triage.utils.AIConstants.MADICAL_TRIAGE_SYSTEM;
+import static com.dd.ai_medical_triage.utils.AIConstants.TOOL_CALLING;
+
 @Configuration
 public class AIConfig {
+
+    public final ChatMemory chatMemory;
+
+    public AIConfig(ChatMemory chatMemory) {
+        this.chatMemory = chatMemory;
+    }
+
     @Bean
     public ChatClient chatClient(OpenAiChatModel model) {
         return ChatClient
                 .builder(model)
+                .defaultSystem(MADICAL_TRIAGE_SYSTEM)
+                .defaultSystem(TOOL_CALLING)
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .build();
 
     }
---
+++ src/main/java/com/dd/ai_medical_triage/utils/AIConstants.java
@@ -0,0 +1,7 @@
+package com.dd.ai_medical_triage.utils;
+
+public class AIConstants {
+
+    public static final String MADICAL_TRIAGE_SYSTEM = "你是一个智能医疗问诊指导助手";
+    public static final String TOOL_CALLING = "如果你需要使用工具，请使用工具进行查询";
+}
