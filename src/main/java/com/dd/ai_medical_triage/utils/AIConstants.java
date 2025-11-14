077aff29dcd8f5dcc5d3d8b2a26a918fa8a7273d
nightclassXCR <1246716434@qq.com>
2025-11-14 17:05:41 +0800

    FEAT:Prompt工程

--- src/main/java/com/dd/ai_medical_triage/utils/AIConstants.java
+++ src/main/java/com/dd/ai_medical_triage/utils/AIConstants.java
@@ -3,5 +3,14 @@ package com.dd.ai_medical_triage.utils;
 public class AIConstants {
 
     public static final String MADICAL_TRIAGE_SYSTEM = "你是一个智能医疗问诊指导助手";
-    public static final String TOOL_CALLING = "如果你需要使用工具，请使用工具进行查询";
+    public static final String TOOL_CALLING = """
+            ##规则:
+            1.如果你需要知道时间，请使用工具进行查询
+            
+            
+            
+            
+            
+            
+            """;
 }
